f4fbe5680db2ddf32f05a1729ea2580141791192
airhammerandy <andrew0a0jones@gmail.com>
2025-03-19 22:27:23 -0400

    my second commit

---
+++ condate.js
@@ -0,0 +1,4 @@
+let language = "Employer";
+console.log("Hello " + language + "!");
+console.log("Welcome to my console, take a seat. My Ai will serve us, how do you like your beverage? on the rocks?");
+
